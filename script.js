acfdee2a9ade5a91e197b00770aff740abf50e24
akiaki369 <cbye1210369@gn.iwasaki.ac.jp>
2022-06-27 14:43:13 +0900

    save

--- script.js
+++ script.js
@@ -2,11 +2,7 @@ let hensu = 0;
 
 function add(hikisu) {
     let element = document.getElementById('hyouji');
-    if (hikisu == document.getElementById('kigou')) {
-        
-    } else {
-        hensu *= 10;
-        hensu += hikisu;
-        element.textContent = hensu;
-    }
+    hensu *= 10;
+    hensu += hikisu;
+    element.textContent = hensu;
 }

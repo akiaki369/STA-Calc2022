a7535b35439bf87a10243ee7ebe5f65528a7c814
akiaki369 <cbye1210369@gn.iwasaki.ac.jp>
2022-06-28 11:20:47 +0900

    save

--- script.js
+++ script.js
@@ -1,63 +1,77 @@
-let hensu = 0;
+let number = 0;
 
 const stack = [];
 
-function addnum(num) {
-    let element = document.getElementById('hyouji');
-    hensu *= 10;
-    hensu += num;
-    element.innerHTML = hensu;
+function getElement() {
+    let element = document.getElementById('display');
+    return element;
+}
+
+function addNum(num) {
+    element = getElement();
+    number *= 10;
+    number += num;
+    element.innerHTML = number;
 }
 
 function ok() {
-    stack.push(hensu);
-    let element = document.getElementById('hyouji');
+    stack.push(number);
+    element = getElement();
     element.innerHTML = 0;
-    hensu = 0;
+    number = 0;
 }
 
 function plus() {
-    let keisan1 = stack.pop();
-    let keisan2 = stack.pop();
-    let tochu = keisan1 + keisan2;
-    stack.push(tochu);
-    let element = document.getElementById('hyouji');
+    let number1 = stack.pop();
+    let number2 = stack.pop();
+    let ans = number1 + number2;
+    stack.push(ans);
+    element = getElement();
     element.innerHTML = 0;
 }
 
 function minus() {
-    let keisan1 = stack.pop();
-    let keisan2 = stack.pop();
-    let tochu = keisan2 - keisan1;
-    stack.push(tochu);
-    let element = document.getElementById('hyouji');
+    let number1 = stack.pop();
+    let number2 = stack.pop();
+    let ans = number2 - number1;
+    stack.push(ans);
+    element = getElement();
     element.innerHTML = 0;
 }
 
 function multiple() {
-    let keisan1 = stack.pop();
-    let keisan2 = stack.pop();
-    let tochu = keisan1 * keisan2;
-    stack.push(tochu);
-    let element = document.getElementById('hyouji');
+    let number1 = stack.pop();
+    let number2 = stack.pop();
+    let ans = number1 * number2;
+    stack.push(ans);
+    element = getElement();
     element.innerHTML = 0;
 }
 
 function divide() {
-    let keisan1 = stack.pop();
-    let keisan2 = stack.pop();
-    let tochu = keisan1 / keisan2;
-    stack.push(tochu);
-    let element = document.getElementById('hyouji');
+    let number1 = stack.pop();
+    let number2 = stack.pop();
+    let ans = number2 / number1;
+    stack.push(ans);
+    element = getElement();
+    element.innerHTML = 0;
+}
+
+function mod() {
+    let number1 = stack.pop();
+    let number2 = stack.pop();
+    let ans = number2 % number1;
+    stack.push(ans);
+    element = getElement();
     element.innerHTML = 0;
 }
 
 function equal() {
-    let element = document.getElementById('hyouji');
+    element = getElement();
     element.innerHTML = stack.pop();
 }
 
 function allclear() {
-    let element = document.getElementById('hyouji');
+    element = getElement();
     element.innerHTML = 0;
 }
